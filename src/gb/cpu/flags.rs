442b6f69d630808f27a00540e6e289cd9456715f
John Kaushagen <johnkaushagen@gmail.com>
2026-04-06 15:23:37 -0400

    make flag members pub and give them better names

--- src/gb/cpu/flags.rs
+++ src/gb/cpu/flags.rs
@@ -5,17 +5,17 @@ const CFLAG: u8 = 1 << 4;
 
 #[derive(Debug, Default, Copy, Clone)]
 pub struct Flags {
-    z: bool,
-    n: bool,
-    h: bool,
-    c: bool,
+    pub zero: bool,
+    pub subtract: bool,
+    pub halfcarry: bool,
+    pub carry: bool,
 }
 
 impl From<Flags> for u8 {
     fn from(value: Flags) -> Self {
-        (value.z as u8 * ZFLAG)
-            | (value.n as u8 * NFLAG)
-            | (value.h as u8 * HFLAG)
-            | (value.c as u8 * CFLAG)
+        (value.zero as u8 * ZFLAG)
+            | (value.subtract as u8 * NFLAG)
+            | (value.halfcarry as u8 * HFLAG)
+            | (value.carry as u8 * CFLAG)
     }
 }
