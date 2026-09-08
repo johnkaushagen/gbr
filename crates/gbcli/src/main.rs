d776873bbf7d1afba27d0dab74dc6be70f5be91b
John Kaushagen <johnkaushagen@gmail.com>
2026-09-07 23:30:40 -0400

    add simple file reading

--- crates/gbcli/src/main.rs
+++ crates/gbcli/src/main.rs
@@ -1,3 +1,35 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        println!("Usage: gbcli path/to/rom");
+        return;
+    }
+    let rom_path = args[1].as_str();
+    let mut rom = match File::open(rom_path) {
+        Ok(f) => f,
+        Err(e) => {
+            eprintln!("Could not open file \"{rom_path}\": {e}");
+            return;
+        }
+    };
+    let mut buffer: Vec<u8> = Vec::new();
+    let nbytes = match rom.read_to_end(&mut buffer) {
+        Ok(n) => n,
+        Err(e) => {
+            eprintln!("Error reading file \"{rom_path}\": {e}");
+            return;
+        }
+    };
+    println!("Read {nbytes} bytes from file \"{rom_path}\"");
+
+    let mut pc = 0;
+    while pc < nbytes {
+        let opcode = buffer[pc];
+        println!("{opcode:#04x} \t\t // {pc:#06x}");
+        pc += 1;
+    }
 }
